5a1f005315c7532c3fe3bb07a6f9ac65fdc24dc1
Richard Saethang <richard.saethang@gmail.com>
2020-04-01 19:50:31 -0700

    moves initializing of data into own method in GildedRose class

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,10 @@ export default class GildedRose {
 
   constructor() {
     this.items = [];
+    this.initializeData();
+  }
+
+  initializeData() {
     this.items.push(new Item('+5 Dexterity Vest', 10, 20));
     this.items.push(new Item('Aged Brie', 2, 0));
     this.items.push(new Item('Elixir of the Mongoose', 5, 7));
